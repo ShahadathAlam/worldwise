fe320d73df9fcd8c3ef330c514556dff765d6328
ShahadathAlam <shahadathalam@ymail.com>
2024-09-06 20:41:14 +0600

    Some chages made on Button component & Form component, BackButton is created and finally finishing the City view

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,33 +1,12 @@
 import styles from "./Button.module.css";
 // console.log(styles["btn"]);
 
-function Button({ children, type, navigate }) {
+function Button({ children, onClick, type }) {
   return (
-    <button
-      className={`${styles.btn} ${styles[type]}`}
-      type="button"
-      onClick={(e) => {
-        e.preventDefault();
-        navigate(-1);
-      }}
-    >
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
       {children}
     </button>
   );
 }
-
-// function Button({ children, type, navigate }) {
-//   return (
-//     <button
-//       className={`${styles.btn} ${styles[type]}`}
-//       type="button"
-//       onClick={() => {
-//         navigate(-1);
-//       }}
-//     >
-//       {children}
-//     </button>
-//   );
-// }
 
 export default Button;
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -16,13 +17,17 @@ const flagemojiToPNG = (flag) => {
     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
   );
 };
+
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
   // console.log(city);
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
